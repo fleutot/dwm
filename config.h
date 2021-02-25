e34df4b3788391f396d0ef0b5ffdc96df66c0693
Gauthier Östervall <gauthier.ostervall@lumenradio.com>
2021-02-25 15:15:03 +0100

    Use generic identifier for launcher command

--- config.h
+++ config.h
@@ -56,14 +56,14 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] =
+static const char *launchercmd[] =
     {"rofi", "-show",  "combi", "-combi-modi", "drun,run", "-modi", "drun,run,ssh", "-config", "/home/gauthier/.xmonad/rofi.conf", NULL };
 
-static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *termcmd[]  = { "urxvt", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = launchercmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
